e2280b456da4e0d4f3466349f18599e46872d67b
Michael Bentley <mikebentley15@gmail.com>
2017-07-10 17:02:49 -0600

    go back to linking against libmfem.a

--- examples/flit-tests/custom.mk
+++ examples/flit-tests/custom.mk
@@ -3,53 +3,53 @@
 
 # additional source files to compile other than what is in '.' and 'tests/'
 # since those directories are added by a wildcard.
-SOURCE         += ../../fem/bilinearform.cpp
-SOURCE         += ../../fem/bilininteg.cpp
-SOURCE         += ../../fem/coefficient.cpp
-SOURCE         += ../../fem/datacollection.cpp
-SOURCE         += ../../fem/eltrans.cpp
-SOURCE         += ../../fem/fe.cpp
-SOURCE         += ../../fem/fe_coll.cpp
-SOURCE         += ../../fem/fespace.cpp
-SOURCE         += ../../fem/geom.cpp
-SOURCE         += ../../fem/gridfunc.cpp
-SOURCE         += ../../fem/hybridization.cpp
-SOURCE         += ../../fem/intrules.cpp
-SOURCE         += ../../fem/linearform.cpp
-SOURCE         += ../../fem/lininteg.cpp
-SOURCE         += ../../fem/nonlinearform.cpp
-SOURCE         += ../../fem/nonlininteg.cpp
-SOURCE         += ../../fem/staticcond.cpp        # 17/24 of the fem files
-SOURCE         += ../../general/array.cpp
-SOURCE         += ../../general/error.cpp
-SOURCE         += ../../general/gzstream.cpp
-SOURCE         += ../../general/socketstream.cpp
-SOURCE         += ../../general/stable3d.cpp
-SOURCE         += ../../general/table.cpp
-SOURCE         += ../../general/tic_toc.cpp       #  7/12 of the general files
-SOURCE         += ../../linalg/blockmatrix.cpp
-SOURCE         += ../../linalg/blockoperator.cpp
-SOURCE         += ../../linalg/blockvector.cpp
-SOURCE         += ../../linalg/densemat.cpp
-SOURCE         += ../../linalg/matrix.cpp
-SOURCE         += ../../linalg/ode.cpp
-SOURCE         += ../../linalg/operator.cpp
-SOURCE         += ../../linalg/solvers.cpp
-SOURCE         += ../../linalg/sparsemat.cpp
-SOURCE         += ../../linalg/sparsesmoothers.cpp
-SOURCE         += ../../linalg/vector.cpp         # 11/17 of the linalg files
-SOURCE         += ../../mesh/element.cpp
-SOURCE         += ../../mesh/hexahedron.cpp
-SOURCE         += ../../mesh/mesh.cpp
-SOURCE         += ../../mesh/mesh_readers.cpp
-SOURCE         += ../../mesh/ncmesh.cpp
-SOURCE         += ../../mesh/nurbs.cpp
-SOURCE         += ../../mesh/point.cpp
-SOURCE         += ../../mesh/quadrilateral.cpp
-SOURCE         += ../../mesh/segment.cpp
-SOURCE         += ../../mesh/tetrahedron.cpp
-SOURCE         += ../../mesh/triangle.cpp
-SOURCE         += ../../mesh/vertex.cpp           # 12/16 of the mesh files
+#SOURCE         += ../../fem/bilinearform.cpp
+#SOURCE         += ../../fem/bilininteg.cpp
+#SOURCE         += ../../fem/coefficient.cpp
+#SOURCE         += ../../fem/datacollection.cpp
+#SOURCE         += ../../fem/eltrans.cpp
+#SOURCE         += ../../fem/fe.cpp
+#SOURCE         += ../../fem/fe_coll.cpp
+#SOURCE         += ../../fem/fespace.cpp
+#SOURCE         += ../../fem/geom.cpp
+#SOURCE         += ../../fem/gridfunc.cpp
+#SOURCE         += ../../fem/hybridization.cpp
+#SOURCE         += ../../fem/intrules.cpp
+#SOURCE         += ../../fem/linearform.cpp
+#SOURCE         += ../../fem/lininteg.cpp
+#SOURCE         += ../../fem/nonlinearform.cpp
+#SOURCE         += ../../fem/nonlininteg.cpp
+#SOURCE         += ../../fem/staticcond.cpp        # 17/24 of the fem files
+#SOURCE         += ../../general/array.cpp
+#SOURCE         += ../../general/error.cpp
+#SOURCE         += ../../general/gzstream.cpp
+#SOURCE         += ../../general/socketstream.cpp
+#SOURCE         += ../../general/stable3d.cpp
+#SOURCE         += ../../general/table.cpp
+#SOURCE         += ../../general/tic_toc.cpp       #  7/12 of the general files
+#SOURCE         += ../../linalg/blockmatrix.cpp
+#SOURCE         += ../../linalg/blockoperator.cpp
+#SOURCE         += ../../linalg/blockvector.cpp
+#SOURCE         += ../../linalg/densemat.cpp
+#SOURCE         += ../../linalg/matrix.cpp
+#SOURCE         += ../../linalg/ode.cpp
+#SOURCE         += ../../linalg/operator.cpp
+#SOURCE         += ../../linalg/solvers.cpp
+#SOURCE         += ../../linalg/sparsemat.cpp
+#SOURCE         += ../../linalg/sparsesmoothers.cpp
+#SOURCE         += ../../linalg/vector.cpp         # 11/17 of the linalg files
+#SOURCE         += ../../mesh/element.cpp
+#SOURCE         += ../../mesh/hexahedron.cpp
+#SOURCE         += ../../mesh/mesh.cpp
+#SOURCE         += ../../mesh/mesh_readers.cpp
+#SOURCE         += ../../mesh/ncmesh.cpp
+#SOURCE         += ../../mesh/nurbs.cpp
+#SOURCE         += ../../mesh/point.cpp
+#SOURCE         += ../../mesh/quadrilateral.cpp
+#SOURCE         += ../../mesh/segment.cpp
+#SOURCE         += ../../mesh/tetrahedron.cpp
+#SOURCE         += ../../mesh/triangle.cpp
+#SOURCE         += ../../mesh/vertex.cpp           # 12/16 of the mesh files
 
 # for when cuda is compiled, you can specify different source files
 CUSOURCE       +=
@@ -66,7 +66,7 @@ CC_REQUIRED    += -I../..
 #   LD_REQUIRED += -L<library-path> -l<library-name>
 # or rpath
 #   LD_REQUIRED += -Wl,-rpath=<abs-path-to-library-dir>
-#LD_REQUIRED    += -L../.. -lmfem
+LD_REQUIRED    += -L../.. -lmfem
 
 # compiler and linker flags respectively - specifically for a dev build
 # - DEV_CFLAGS:   non-recorded compiler flags (such as includes)
