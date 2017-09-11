73d0023cce3fa277e4e9f6ca43ec392ca8e41efa
Michael Bentley <mbentley@cs.utah.edu>
2017-09-11 17:21:16 -0600

    reset the experiment for use with icpc

--- examples/flit-tests-ex8/investigate.mk
+++ examples/flit-tests-ex8/investigate.mk
@@ -17,8 +17,8 @@ CFLAGS         += -Wno-shift-count-overflow
 GT_OPTL        := -O0
 GT_SWITCHES    :=
 GT_CFLAGS      := $(GT_OPTL) $(GT_SWITCHES)
-DEV_OPTL       := -O3
-DEV_SWITCHES   := -funsafe-math-optimizations
+DEV_OPTL       := -O2
+DEV_SWITCHES   :=
 DEV_CFLAGS     := $(DEV_OPTL) $(DEV_SWITCHES)
 
 LFLAGS         := -lm
@@ -32,56 +32,54 @@ TARGET         := investigaterun
 TARGET_OUT     := investigaterun.csv
 TARGET_DAT     := $(TARGET_OUT:%=%_Example08_d.dat)
 DAT_COMPARE    := devrun.csv_Example08_d.dat
+DEV_TARGET     := devtarget
 
 DEV_SRC        :=
 GT_SRC         :=
 
-GT_SRC         += main.cpp
-GT_SRC         += tests/Example08.cpp
-GT_SRC         += ../../fem/bilinearform.cpp
-GT_SRC         += ../../fem/bilininteg.cpp
-GT_SRC         += ../../fem/coefficient.cpp
-GT_SRC         += ../../fem/eltrans.cpp
-GT_SRC         += ../../fem/fe.cpp
-GT_SRC         += ../../fem/fe_coll.cpp
-GT_SRC         += ../../fem/fespace.cpp
-GT_SRC         += ../../fem/geom.cpp
-GT_SRC         += ../../fem/gridfunc.cpp
-GT_SRC         += ../../fem/hybridization.cpp
-GT_SRC         += ../../fem/intrules.cpp
-GT_SRC         += ../../fem/linearform.cpp
-GT_SRC         += ../../fem/lininteg.cpp
-GT_SRC         += ../../fem/nonlininteg.cpp
-GT_SRC         += ../../fem/staticcond.cpp
-GT_SRC         += ../../general/array.cpp
-GT_SRC         += ../../general/error.cpp
-GT_SRC         += ../../general/gzstream.cpp
-GT_SRC         += ../../general/stable3d.cpp
-GT_SRC         += ../../general/socketstream.cpp
-GT_SRC         += ../../general/table.cpp
-GT_SRC         += ../../linalg/blockoperator.cpp
-GT_SRC         += ../../linalg/blockvector.cpp
-GT_SRC         += ../../linalg/densemat.cpp
-GT_SRC         += ../../linalg/matrix.cpp
-GT_SRC         += ../../linalg/operator.cpp
-GT_SRC         += ../../linalg/solvers.cpp
-GT_SRC         += ../../linalg/sparsemat.cpp
-
-GT_SRC         += ../../linalg/vector-gt.cpp
-DEV_SRC        += ../../linalg/vector-dev.cpp
-
-GT_SRC         += ../../mesh/element.cpp
-GT_SRC         += ../../mesh/hexahedron.cpp
-GT_SRC         += ../../mesh/mesh.cpp
-GT_SRC         += ../../mesh/mesh_readers.cpp
-GT_SRC         += ../../mesh/ncmesh.cpp
-GT_SRC         += ../../mesh/nurbs.cpp
-GT_SRC         += ../../mesh/point.cpp
-GT_SRC         += ../../mesh/quadrilateral.cpp
-GT_SRC         += ../../mesh/segment.cpp
-GT_SRC         += ../../mesh/tetrahedron.cpp
-GT_SRC         += ../../mesh/triangle.cpp
-GT_SRC         += ../../mesh/vertex.cpp
+DEV_SRC         += main.cpp
+DEV_SRC         += tests/Example08.cpp
+DEV_SRC         += ../../fem/bilinearform.cpp
+DEV_SRC         += ../../fem/bilininteg.cpp
+DEV_SRC         += ../../fem/coefficient.cpp
+DEV_SRC         += ../../fem/eltrans.cpp
+DEV_SRC         += ../../fem/fe.cpp
+DEV_SRC         += ../../fem/fe_coll.cpp
+DEV_SRC         += ../../fem/fespace.cpp
+DEV_SRC         += ../../fem/geom.cpp
+DEV_SRC         += ../../fem/gridfunc.cpp
+DEV_SRC         += ../../fem/hybridization.cpp
+DEV_SRC         += ../../fem/intrules.cpp
+DEV_SRC         += ../../fem/linearform.cpp
+DEV_SRC         += ../../fem/lininteg.cpp
+DEV_SRC         += ../../fem/nonlininteg.cpp
+DEV_SRC         += ../../fem/staticcond.cpp
+DEV_SRC         += ../../general/array.cpp
+DEV_SRC         += ../../general/error.cpp
+DEV_SRC         += ../../general/gzstream.cpp
+DEV_SRC         += ../../general/stable3d.cpp
+DEV_SRC         += ../../general/socketstream.cpp
+DEV_SRC         += ../../general/table.cpp
+DEV_SRC         += ../../linalg/blockoperator.cpp
+DEV_SRC         += ../../linalg/blockvector.cpp
+DEV_SRC         += ../../linalg/densemat.cpp
+DEV_SRC         += ../../linalg/matrix.cpp
+DEV_SRC         += ../../linalg/operator.cpp
+DEV_SRC         += ../../linalg/solvers.cpp
+DEV_SRC         += ../../linalg/sparsemat.cpp
+DEV_SRC         += ../../linalg/vector.cpp
+DEV_SRC         += ../../mesh/element.cpp
+DEV_SRC         += ../../mesh/hexahedron.cpp
+DEV_SRC         += ../../mesh/mesh.cpp
+DEV_SRC         += ../../mesh/mesh_readers.cpp
+DEV_SRC         += ../../mesh/ncmesh.cpp
+DEV_SRC         += ../../mesh/nurbs.cpp
+DEV_SRC         += ../../mesh/point.cpp
+DEV_SRC         += ../../mesh/quadrilateral.cpp
+DEV_SRC         += ../../mesh/segment.cpp
+DEV_SRC         += ../../mesh/tetrahedron.cpp
+DEV_SRC         += ../../mesh/triangle.cpp
+DEV_SRC         += ../../mesh/vertex.cpp
 
 SOURCE         := $(DEV_SRC)
 SOURCE         += $(GT_SRC)
@@ -93,22 +91,31 @@ GT_OBJ         := $(addprefix $(OBJ_DIR)/,$(notdir $(GT_SRC:%.cpp=%_gt.o)))
 OBJ            := $(DEV_OBJ)
 OBJ            += $(GT_OBJ)
 
+DEV_TARGET_OBJ := $(addprefix $(OBJ_DIR)/,$(notdir $(SOURCE:%.cpp=%_dev.o)))
+
 DEV_DEP        := $(DEV_OBJ:%.o=%.d)
 GT_DEP         := $(GT_OBJ:%.o=%.d)
 DEPS           := $(DEV_DEP)
 DEPS           += $(GT_DEP)
 
 .PHONY: all run target
-all: diff target
+all: diff target dev
 target: $(TARGET)
-diff: $(TARGET_DAT)
+dev: $(DEV_TARGET)
+diff: $(TARGET_DAT) $(DAT_COMPARE)
 	diff -q $(DAT_COMPARE) $(TARGET_DAT)
+
+$(TARGET): $(OBJ) investigate.mk
+	$(LINK_CC) $(CFLAGS) -o $(TARGET) $(OBJ) $(LFLAGS)
 
 $(TARGET_DAT): $(TARGET)
 	./$(TARGET) --timing-repeats 1 --timing-loops 1 --output $(TARGET_OUT) 2>&1 >/dev/null
 
-$(TARGET): $(OBJ) investigate.mk
-	$(LINK_CC) $(CFLAGS) -o $(TARGET) $(OBJ) $(LFLAGS)
+$(DEV_TARGET): $(DEV_TARGET_OBJ) investigate.mk
+	$(LINK_CC) $(CFLAGS) -o $(DEV_TARGET) $(DEV_TARGET_OBJ) $(LFLAGS)
+
+$(DAT_COMPARE): $(DEV_TARGET)
+	./$(DEV_TARGET) --timing-repeats 1 --timing-loops 1 --output $(DAT_COMPARE) 2>&1 >/dev/null
 
 $(OBJ_DIR):
 	mkdir -p $(OBJ_DIR)
